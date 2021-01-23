dc6befe7b1b0874988be673f29c56a8f13074abc
David De Matteo <74244303+hirnworx@users.noreply.github.com>
2021-01-23 20:20:37 +0100

    FELDMANN UI
    FELDMANN UI DESIGN

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { MenuController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { TranslateConfigService } from './translate-config.service';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,38 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  currentRoute:any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private translateConfigService: TranslateConfigService,
+    private menu: MenuController
   ) {
     this.initializeApp();
   }
 
+  systemLanguage:any;
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString("#8E54E9");
       this.splashScreen.hide();
+      this.updateAppLanguage();
     });
+  }
+
+  updateAppLanguage(){
+    if(!localStorage.getItem('appLanguage')){
+      localStorage.setItem('appLanguage', "en");
+      this.systemLanguage = "en";
+    }else{
+     this.systemLanguage = localStorage.getItem('appLanguage');
+    }
+    this.translateConfigService.setLanguage(this.systemLanguage);
+  } 
+
+  closeMenu(){
+    this.menu.close('first');
   }
 }
